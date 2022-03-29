d48d0a73dbfeaafa5ab7a5edd377c763357be34d
vlskibern <fongshua@gmail.com>
2022-03-29 16:00:03 +0200

    added card styling and more api updates

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-let namesArray = []
+let characterArray = []
 
-async function getNames(url) {
-  const name = await fetch(url)
-  const jsondata = await name.json()
-  jsondata.forEach(name => {
-    namesArray.push(name)
+async function getcharacters(url) {
+  const character = await fetch(url)
+  const jsondata = await character.json()
+  jsondata.forEach(character => {
+    characterArray.push(character)
 })
 }
 
-function listNames(){
-  const name = document.getElementById("name")
-  for(let i = 0; i < namesArray.length; i++){
-    name.innerHTML += `<li id="${i}"> <h3>${namesArray[i].name}</h3></li>`
+function listcharacters(){
+  const character = document.getElementById("name")
+  for(let i = 0; i < characterArray.length; i++){
+    if(characterArray[i].alive == false){
+    character.innerHTML += 
+    `<div class="card">
+    <img class="pfp" src="${characterArray[i].image}"</img>
+      <div class="container">
+        <h1>${characterArray[i].name}</h1>
+        <h1>${characterArray[i].house}</h1>
+        <h1>${characterArray[i].yearOfBirth}</h1>
+        <h3>Dead</h3>
+      </div>
+    </div>`
+  }
+  else {
+    character.innerHTML += 
+    `<li id="${i}"> 
+    <div class="card">
+    <img class="pfp" src="${characterArray[i].image}"</img>
+      <div class="container">
+        <h1>${characterArray[i].name}</h1>
+        <h1>${characterArray[i].house}</h1>
+        <h1>${characterArray[i].yearOfBirth}</h1>
+        <h2>Alive</h2>
+      </div>
+    </div>`
   }
 
 }
+  
 
-getNames("http://hp-api.herokuapp.com/api/characters").then(() => {
-  listNames()
+}
+
+getcharacters("http://hp-api.herokuapp.com/api/characters").then(() => {
+  listcharacters()
 });
 
 
-console.log(namesArray)
+console.log(characterArray)
